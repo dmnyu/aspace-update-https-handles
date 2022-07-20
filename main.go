6d1fe92bfff490af6389a68f84e71ad5cb21db53
Donald Mennerich <don.mennerich@nyu.edu>
2022-07-20 16:01:17 -0400

    Updating logic

--- main.go
+++ main.go
@@ -9,76 +9,67 @@ import (
 	"strings"
 )
 
+var client *aspace.ASClient
+
 func main() {
-	client, err := aspace.NewClient("go-aspace.yml", "dev", 20)
-	if err != nil {
-		panic(err)
-	}
-
-	iresults, err := client.Search(2, "digital_object", "*", 1)
-	if err != nil {
-		panic(err)
-	}
-
-	outFile, _ := os.Create("handles.txt")
+	outFile, _ := os.Create("handle-updates.txt")
 	defer outFile.Close()
 	log.SetOutput(outFile)
 
-	for i := 1; i <= iresults.LastPage; i++ {
-		fmt.Printf("Page %d of %d\n", i, iresults.LastPage)
-		numUpdates := 0
-		page, err := client.Search(3, "digital_object", "*", i)
+	var err error
+	client, err = aspace.NewClient("go-aspace.yml", "dev", 20)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, i := range []int{2, 3, 6} {
+		getDOs(i)
+	}
+}
+
+func getDOs(repoID int) {
+	iresults, err := client.Search(repoID, "digital_object", "*", 1)
+	if err != nil {
+		panic(err)
+	}
+
+	for page := 1; page <= iresults.LastPage; page++ {
+		fmt.Printf("Repository %d Page %d of %d\n", repoID, page, iresults.LastPage)
+
+		pageResults, err := client.Search(repoID, "digital_object", "*", page)
 		if err != nil {
 			panic(err)
 		}
-
-		for _, result := range page.Results {
+		for _, result := range pageResults.Results {
 			do := aspace.DigitalObject{}
 			err = json.Unmarshal([]byte(fmt.Sprint(result["json"])), &do)
 			if err != nil {
 				panic(err)
 			}
+			if containsHandle(do.FileVersions) == true {
+				//repoID, doID, err := aspace.URISplit(do.URI)
 
-			repoID, doID, err := aspace.URISplit(do.URI)
-			if err != nil {
-				panic(err)
-			}
-
-			fileVersions := do.FileVersions
-			if containsHandle(fileVersions) == true {
-				numUpdates = numUpdates + 1
-				do.FileVersions = updateFileVersions(fileVersions)
-				msg, err := client.UpdateDigitalObject(repoID, doID, do)
-				if err != nil {
-					log.Printf("[ERROR] %s\n", strings.ReplaceAll(err.Error(), "\n", ""))
-					continue
+				newFV := []aspace.FileVersion{}
+				for _, fv := range do.FileVersions {
+					if strings.Contains(fv.FileURI, "http://hdl.handle.net/2333.1/") == true {
+						fv.FileURI = strings.ReplaceAll(fv.FileURI, "http", "https")
+						newFV = append(newFV, fv)
+					} else {
+						newFV = append(newFV, fv)
+					}
 				}
-				log.Printf("[INFO] %s\n", strings.ReplaceAll(msg, "\n", ""))
+				do.FileVersions = newFV
+				fmt.Printf("[INFO] %s %v\n", do.URI, do.FileVersions)
 			}
 		}
-
-		fmt.Println(numUpdates, " DOs updated")
 	}
 }
 
 func containsHandle(fileVersions []aspace.FileVersion) bool {
 	for _, fv := range fileVersions {
-		if strings.Contains(fv.FileURI, "hdl.handle") == true {
+		if strings.Contains(fv.FileURI, "http://hdl.handle.net/2333.1/") == true {
 			return true
 		}
 	}
 	return false
-}
-
-func updateFileVersions(fileVersions []aspace.FileVersion) []aspace.FileVersion {
-	newFileVersions := []aspace.FileVersion{}
-	for _, fv := range fileVersions {
-		if strings.Contains(fv.FileURI, "hdl.handle") == true {
-			fv.FileURI = strings.ReplaceAll(fv.FileURI, "http", "https")
-			newFileVersions = append(fileVersions, fv)
-		} else {
-			newFileVersions = append(fileVersions, fv)
-		}
-	}
-	return newFileVersions
 }
